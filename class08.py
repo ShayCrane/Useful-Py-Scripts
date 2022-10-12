92a09c4a7752cc8f14cd5cf9a9082b5bfc2c69bf
shannon <shannon@ubuntudev>
2022-10-12 23:33:50 +0000

    first commit 1.2

--- class08.py
+++ class08.py
@@ -6,42 +6,47 @@
 # Date of last revision: 10/12/2022
 # Purpose: an exercise to simulate a ransomware attack: 
 #          - a final update to the class06.py script
-#          - that adds an option giving us the ability to add a ransomware note 
-#          - that is visible to the Windows user, after the data is encrypted.
-
-# Requirements
-# Add a feature capability to your Python encryption tool to:
-
-    # Optional if possible: Alter the desktop wallpaper on a Windows PC with a ransomware message
-    # Create a popup window on a Windows PC with a ransomware message
-
-# Make this feature optional. In the user menu prompt, add this as a ransomware simulation option.
-
-# Stretch Goals (Optional Objectives)
-# Add additional features that draw the computer user’s attention to the ransomware message.
 
 
-# redefine ask_user() function (class06.py) to include more options
+# I referred to the following source for components of this script: 
+# https://github.com/ncorbuk/Python-Ransomware/blob/master/RansomWare.py
+
 
 # install pyautogui library
+import ctypes
+import urllib.request
 import pyautogui
-import sys
 
-# define ransom-note function
-def ransom_note():
+# define change_wallpaper function
+def change_wallpaper(plaster):
+    imageUrl = 'https://www.secpod.com/blog/wp-content/uploads/2017/05/Screenshot-from-2017-05-14-23-42-20.png'
+    path = f'{plaster.sysRoot}Desktop/background.jpg'
+    urllib.request.urlretrieve(imageUrl, path)
+    SPI_SETDESKWALLPAPER = 20
+    ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, path, 0)
+
+def leave_note():
     pyautogui.prompt('You have been hacked!\nEnter passkey to get your files back\nCancel to lose all your files forever', 'Hack Announcement Service', 'Submit Passkey')
 
-# define user_choice function to demonstrate 
-# how ransom_note function fits into the larger script
-def ask_user():
-    user_choice = input('pretend all the options from previous scripts are also here\n Enter 10 to simulate ransomware attack: ')
+
+# define ransom-note function
+def ransomware_simulate():
+    change_wallpaper()
+    leave_note()
     
-    if (user_choice == '10'): 
-        ransom_note()
+# redefine ask_user() function (class06.py) to include more options
+# and to demonstrate how it fits into the larger script
+def ask_user():
+    user_choice = input('pretend all the options from previous scripts are also here\n Enter 8 to simulate ransomware attack: ')
+    
+    if (user_choice == '8'): 
+        ransomware_simulate()
     else: 
-        sys.exit()
+        print('You can only enter 8 to proceed. Try again.')
 
 # Main
 # invoke the ask_user function
 ask_user()
+
+# End
 
